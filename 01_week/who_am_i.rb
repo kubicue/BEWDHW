14d0ff066ad7c193ecf6643534e77498a181fbf9
kubicue <3x2media@gmail.com>
2015-09-12 20:34:30 -0700

    who_am_i.rb file

---
+++ 01_week/who_am_i.rb
@@ -0,0 +1,8 @@
+# Who am I
+my_name = "Jonathan Figueroa"
+grew_up = "San Juan, Puerto Rico"
+current_job = "Web Developer"
+hidden_talent = "Cook"
+
+# Print output
+puts "My name is #{my_name}, I grew up in #{grew_up}. I currently work as a #{current_job} and I like to #{hidden_talent}."
