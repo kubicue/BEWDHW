50d884f578bb022ffbec9e430543a21dc757bcc0
kubicue <3x2media@gmail.com>
2015-09-12 20:40:03 -0700

    who_am_i.rb plus bonus

--- 01_week/who_am_i.rb
+++ 01_week/who_am_i.rb
@@ -6,3 +6,6 @@ hidden_talent = "Cook"
 
 # Print output
 puts "My name is #{my_name}, I grew up in #{grew_up}. I currently work as a #{current_job} and I like to #{hidden_talent}."
+
+# Print downcase
+puts "My name is #{my_name.downcase}, I grew up in #{grew_up.downcase}. I currently work as a #{current_job.downcase} and I like to #{hidden_talent.downcase}."
