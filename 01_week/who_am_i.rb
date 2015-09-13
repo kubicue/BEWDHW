cc5f1b45dd91b7e57edbda5baf4c7e48d883aa65
kubicue <3x2media@gmail.com>
2015-09-12 20:48:46 -0700

    who_am_i.rb and Bonus

--- 01_week/who_am_i.rb
+++ 01_week/who_am_i.rb
@@ -1,8 +1,12 @@
-# Who am I
-my_name = "Jonathan Figueroa"
-grew_up = "San Juan, Puerto Rico"
-current_job = "Web Developer"
-hidden_talent = "Cook"
+# Who I Am
+print "Whats is you Name?"
+my_name = gets.chomp
+print "Where do you grew up?"
+grew_up = gets.chomp
+print "What is your current job title"
+current_job = gets.chomp
+print "Do you have a magic talent?"
+hidden_talent = gets.chomp
 
 # Print output
 puts "My name is #{my_name}, I grew up in #{grew_up}. I currently work as a #{current_job} and I like to #{hidden_talent}."
