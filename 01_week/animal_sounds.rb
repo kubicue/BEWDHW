923117ca35b3981509c9f098c754889ca1ea9885
kubicue <3x2media@gmail.com>
2015-09-12 21:02:31 -0700

    animal_sounds.rb

---
+++ 01_week/animal_sounds.rb
@@ -0,0 +1,8 @@
+# Animal Sounds Program
+print "Enter an animal that makes a sound:"
+animal = gets.chomp
+print "What sound does a chicken make?"
+sounds = gets.chomp*3
+
+# Animal Sound Output
+puts "The chicken goes #{sounds}"
