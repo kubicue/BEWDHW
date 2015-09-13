d39d2457125a1bc9117a0f63bb5ab95af08e10c0
kubicue <3x2media@gmail.com>
2015-09-12 21:05:47 -0700

    animal_sounds.rb

--- 01_week/animal_sounds.rb
+++ 01_week/animal_sounds.rb
@@ -2,7 +2,7 @@
 print "Enter an animal that makes a sound:"
 animal = gets.chomp
 print "What sound does a chicken make?"
-sounds = gets.chomp*3
+sounds = gets.chomp* 3.downcase
 
 # Animal Sound Output
 puts "The chicken goes #{sounds}"
