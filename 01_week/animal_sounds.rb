dd49e63d004fab84fd58e00bb4dbe38c903fd234
kubicue <3x2media@gmail.com>
2015-09-16 16:38:53 -0700

    Homework assigment week one

--- 01_week/animal_sounds.rb
+++ 01_week/animal_sounds.rb
@@ -1,10 +1,21 @@
+=begin
+ Title: Homework for GA BEWD-LA-08 class
+ Name: animal_sounds.rb
+ Author: Jonathan Figueroa
+ URI: https://github.com/kubicue/BEWDHW/blob/master/01_week/animal_sounds.rb
+=end
+
 # Choose an animal
 puts "Enter an animal that makes a sound:"
 my_animal = gets.chomp
-# What sound your animal makes?
-puts "What sound does a #{my_animal} make?"
+
+# What sound your animal makes
+puts "What sound does a #{my_animal.downcase} make?"
 animal_sound = gets.chomp
 
+# How many times
+puts "The " + my_animal.downcase + " goes " + animal_sound.downcase.strip + " how many times?"
+sound_times = gets.chomp.to_i
 
-# Animal Sound Output
-puts "The #{my_animal.downcase} goes #{animal_sound.downcase * 3}"
+# Animal Sound Output with string concatnation
+puts "The " + my_animal.downcase + " goes " + (animal_sound.downcase.strip + "," + " ") * (sound_times - 1) + animal_sound.downcase.strip + "."
