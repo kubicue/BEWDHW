d031a59fb4f287edd59d3647f8be693e2b4f7b56
kubicue <3x2media@gmail.com>
2015-09-16 22:51:36 -0700

    Add bonus_challenges

---
+++ bewd-hm/bonus_challenges/ruby_smoothies/smoothies.rb
@@ -0,0 +1,19 @@
+# create a class called Blender
+# # It should have a method that takes an array of ingredients and returns a
+# # mixed string of characters.
+# # # Give the blender an on and off switch and only allow the blender to
+# function when it's on.
+# # # FOR SAFETY'S SAKE When you create a new blender by default it should be
+# off.
+# # # Blend the the smoothie array
+
+# work in progress ...
+class Blender
+
+  def initialize (blender_off, blender_off)
+    # Instance variables
+    @blender_off = blender_off
+    @blender_on = blender_on
+
+  end
+end
