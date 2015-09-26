d9501683c77a4839589f0558f363f7d0a18db937
kubicue <3x2media@gmail.com>
2015-09-26 11:02:52 -0700

    todo_app.rb

---
+++ src/bewd_homework/lab_03/todo.rb
@@ -0,0 +1,6 @@
+puts "What you need to do today?"
+task1 = gets.chomp # task 1
+puts "What you need to do today?"
+task2 = gets.chomp # task 2
+puts "What you need to do today?"
+task3 = gets.chomp # task 3
