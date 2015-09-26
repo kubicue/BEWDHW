cb826fe5c6baec9d2bf62620734019e7a963126b
kubicue <3x2media@gmail.com>
2015-09-26 07:08:59 -0700

    Dir clean_up lab 03 created

--- src/bewd_homework/class_3_4_homework/extra/apt_dec_maker.rb
+++ src/bewd_homework/class_3_4_homework/extra/apt_dec_maker.rb
@@ -4,28 +4,12 @@ puts "Welcome to Apartment Decision Maker"
 puts "We will find you an aparment that accomodate your budget."
 puts "To best fits your needs answer the question with simple Yes or No"
 
-user_feedback = false
+user_feedback = gets.chomp
 
------- work()
-while (not user_feedback)
-      puts "Do you want to look for an aparment? (Yes or No)"
-      answer = gets.chomp.downcase
-
-      if (answer == "yes" or answer == "no")
-        answer = true
-      else
-        puts "Please anwser the question with yes or no."
-    end
-end
-user_feedback = false
-while (not user_feedback)
-      puts "What city do you like to live? ie.[Los Angeles, San Diego]?"
-      answer = gets.chomp.downcase
-
-      if (answer == "yes" or answer == "no")
-        answer = true
-      else
-        puts "Please anwser the question with yes or no."
-
-    end
+while
+  if user_feedback == 'Yes'
+  puts "Do you want to look for an apartment (Yes or No) "
+else  user_feedback == 'No'
+    puts "Thanks for using our service, have nice day!"
+  end
 end
