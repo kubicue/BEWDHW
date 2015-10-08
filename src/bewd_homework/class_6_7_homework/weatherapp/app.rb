4b0cda03dc60743cf06ac53a840b7a956d3e12ca
kubicue <3x2media@gmail.com>
2015-10-07 23:03:44 -0700

    sinatra app clean_up

--- src/bewd_homework/class_6_7_homework/weatherapp/app.rb
+++ src/bewd_homework/class_6_7_homework/weatherapp/app.rb
@@ -1,7 +1,6 @@
 require 'sinatra'
 require 'httparty'
 
-
 get '/' do
   #API call to forcast io [San Diego, Ca]
   @url = HTTParty.get 'https://api.forecast.io/forecast/8b0e1f8b53a5e7e0e6b2a1b2ced3ceda/32.7150,-117.1625'
