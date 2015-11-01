bbd71eead859bca4a09770c6f3ea6a344154264a
kubicue <3x2media@gmail.com>
2015-10-31 17:07:17 -0700

    modify bewd_inventory

--- src/bewd_homework/lab_08/bewd_inventory/app/controllers/products_controller.rb
+++ src/bewd_homework/lab_08/bewd_inventory/app/controllers/products_controller.rb
@@ -5,7 +5,7 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
     @users = User.all
-    @product = Product.find(2)
+
   end
 
   # GET /products/1
@@ -15,32 +15,30 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
-  # GET /products/new
   def new
-    @product = Product.new
+
+  @product = Product.new
+end
+
+def create
+  @product = Product.new(user_params)
+
+  respond_to do |format|
+    if @product.save
+      format.html { redirect_to @product, notice: 'User was successfully created.' }
+      format.json { render :show, status: :created, location: @product }
+    else
+      format.html { render :new }
+      format.json { render json: @product.errors, status: :unprocessable_entity }
+    end
   end
+end
+
 
   # GET /products/1/edit
   def edit
     @product = Product.find(params[:id])
   end
-
-  # POST /products
-  # POST /products.json
-  def create
-    @product = Product.new(product_params)
-
-    respond_to do |format|
-      if @product.save
-        format.html { redirect_to @product, notice: 'Product was successfully created.' }
-        format.json { render :show, status: :created, location: @product }
-      else
-        format.html { render :new }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
   # PATCH/PUT /products/1
   # PATCH/PUT /products/1.json
   def update
